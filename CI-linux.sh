d986560846478d54686d40d5d0e6ad2dec08612e
Kristian Duske <kristian.duske@gmail.com>
2026-04-20 14:18:15 +0200

    Update linuxdeploy and Qt plugin

--- CI-linux.sh
+++ CI-linux.sh
@@ -3,8 +3,8 @@
 # set -o verbose
 
 # install linuxdeploy
-wget -nc https://github.com/linuxdeploy/linuxdeploy/releases/download/1-alpha-20251107-1/linuxdeploy-x86_64.AppImage
-wget -nc https://github.com/linuxdeploy/linuxdeploy-plugin-qt/releases/download/1-alpha-20250213-1/linuxdeploy-plugin-qt-x86_64.AppImage
+wget -nc https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage
+wget -nc https://github.com/linuxdeploy/linuxdeploy-plugin-qt/releases/download/continuous/linuxdeploy-plugin-qt-x86_64.AppImage
 chmod u+x ./linuxdeploy-x86_64.AppImage
 chmod u+x ./linuxdeploy-plugin-qt-x86_64.AppImage
 
